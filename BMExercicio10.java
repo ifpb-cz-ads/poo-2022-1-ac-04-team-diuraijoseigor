52e9603fcbe224e945cecbe0474d2faafa72929f
Jose-Igor <95245791+Jose-Igor@users.noreply.github.com>
2022-06-04 14:08:27 -0300

    Adicionando questão 10

---
+++ BMExercicio10.java
@@ -0,0 +1,9 @@
+public class BMExercicio10 {
+    public static void main(String[] args) {
+        int x = 10;
+        do{
+            x++;
+            System.out.println(x);
+        }while(x < 30);
+    }
+}
