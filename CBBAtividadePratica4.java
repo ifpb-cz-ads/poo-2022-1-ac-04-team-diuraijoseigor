bd431c7e1cc39c6dd01d866c4143d42d22d0fd00
Diuari <95585201+Diuari@users.noreply.github.com>
2022-06-04 14:22:13 -0300

    adicionando questões pares

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica4 {
+    
+    public static void main(String[] args){
+
+
+        for(int k =0;k<=100;k++){
+
+            System.out.println(k);
+        }
+
+    }
+
+
+}
