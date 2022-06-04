06acdcbe12de5210ed51bdf239ba61243a4938cb
Diuari <95585201+Diuari@users.noreply.github.com>
2022-06-04 14:16:50 -0300

    Adicionando questões pares

--- CBBAtividadePratica5.java
+++ CBBAtividadePratica5.java
@@ -1,9 +1,14 @@
-public class CBBAtividadePratica5{
-    public static void main(String[] args) {
-        for(int i = 0; i <= 100; i++){
-            if(i % 2 == 0){
-                System.out.println(i);
-            }
+public class CBBAtividadePratica5 {
+    
+    public static void main(String[] args){
+
+
+        for(int k =0;k<=100;k++){
+
+            System.out.println(k);
         }
+
     }
+
+
 }
